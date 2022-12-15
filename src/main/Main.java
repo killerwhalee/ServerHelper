6b006ba8dbce666ca0975b08f06640d813884552
BuildTools <unconfigured@null.spigotmc.org>
2022-12-15 20:57:54 +0900

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable () {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.BLUE + "Enabled!");
+    }
+
+    @Override
+    public void onDisable () {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Disabled!");
+    }
+}
